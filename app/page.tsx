39be6ce40d6a4984253e2d850fe41193daad02e8
Brian Shimkus <brianshimkus1@gmail.com>
2024-07-02 23:18:18 -0500

    Add transaction form

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import AddTransaction from '@/components/AddTransaction'
 import Guest from '@/components/Guest'
 import { currentUser } from '@clerk/nextjs/server'
 
@@ -11,6 +12,7 @@ export default async function HomePage() {
 	return (
 		<main>
 			<h1>Welcome, {user.firstName}</h1>
+			<AddTransaction />
 		</main>
 	)
 }
--- components/AddTransaction.tsx
+++ components/AddTransaction.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 export default function AddTransaction() {
 	const clientAction = async (formData: FormData) => {
 		console.log(formData.get('text'), formData.get('amount'))
@@ -6,7 +8,25 @@ export default function AddTransaction() {
 	return (
 		<>
 			<h3>Add transaction</h3>
-			<form action={clientAction}></form>
+			<form action={clientAction}>
+				<div className='form-control'>
+					<label htmlFor='text'>Text</label>
+					<input
+						type='text'
+						id='text'
+						name='text'
+						placeholder='Enter text...'
+					/>
+				</div>
+				<div className='form-control'>
+					<label htmlFor='amount'>
+						Amount <br />
+						(negative - expense, positive - income)
+					</label>
+					<input type='number' id='amount' placeholder='Enter amount...' />
+				</div>
+				<button className='btn'>Add transaction</button>
+			</form>
 		</>
 	)
 }
