e623001f1cc991de620c00b77a0138f56957cc61
Brian Shimkus <brianshimkus1@gmail.com>
2024-07-03 23:57:53 -0500

    Commas utility function

--- components/Balance.tsx
+++ components/Balance.tsx
@@ -1,4 +1,5 @@
 import getUserBalance from '@/app/actions/getUserBalance'
+import { addCommas } from '@/lib/utils'
 
 export default async function Balance() {
 	const { balance } = await getUserBalance()
@@ -6,7 +7,7 @@ export default async function Balance() {
 	return (
 		<>
 			<h4>Your Balance</h4>
-			<h1>${balance ?? 0}</h1>
+			<h1>${addCommas(Number(balance?.toFixed(2) ?? 0))}</h1>
 		</>
 	)
 }
